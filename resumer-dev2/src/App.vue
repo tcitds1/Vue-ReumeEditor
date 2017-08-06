d357363f6381c59d2d7c10f52f61093307918c8f
konyahoshi <konyahoshi@qq.com>
2017-08-06 20:06:30 +0800

    添加注册登录对话框

---
+++ resumer-dev2/src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="app">
+    <!--<p>{{text}}</p>-->
+    <div class="page">
+      <header>
+        <Topbar/>
+      </header>
+      <main>
+        <ResumeEditor/>
+        <ResumePreview/>
+      </main>  
+    </div>
+  </div>
+</template>
+<script>
+import './assets/reset.css'
+import 'normalize.css/normalize.css'
+import Topbar from './components/Topbar'
+import ResumeEditor from './components/ResumeEditor'
+import ResumePreview from './components/ResumePreview'
+import icons from './assets/icons'
+import store from './store/index'
+export default {
+  name: 'app',
+  data: function () {
+    return {
+      text: '你好'
+    }
+  },
+  store,
+  components: {Topbar, ResumeEditor, ResumePreview},
+  created () {
+    document.body.insertAdjacentHTML('afterbegin', icons)
+    // 在body开始标签后插入icons
+    let state = localStorage.getItem('state')
+    if (state) {
+      state = JSON.parse(state)
+    }
+    this.$store.commit('initState', state)
+  }
+}
+
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.page {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: #EAEBEC;
+  >main {
+    flex-grow: 1;
+    min-width: 1024px;
+    max-width: 1440px;
+    margin-top:16px;
+    margin-bottom: 16px;
+    display: flex;
+    /*justify-content: space-around;*/
+    justify-content: space-between;
+    padding: 0 16px;
+    align-self: center;
+    width:100%;
+  }
+}
+#resumeEditor {
+  min-width: 35%; //为什么min-width代替width就没有样式bug
+  background: #444;
+}
+#resumePreview {
+  // width: 61.666667%;
+  background: #777;
+  flex-grow: 1;
+  margin-left: 16px;
+}
+svg.icon {
+  height: 1em;
+  width: 1em;
+  fill: currentColor;
+  vertical-align: -0.1em;
+  font-size: 16px;
+}
+</style>
---
+++ resumer-dev2/src/components/Topbar.vue
@@ -0,0 +1,74 @@
+<template>
+    <div id="Topbar">
+        <div class="wrapper">
+            <span class="logo">Resumer</span>
+            <div class="actions">
+                <a href="#" class="button primary" @click.prevent="signUpDialogVisible = true">注册</a>
+                <MyDialog title = "注册" :visible = "signUpDialogVisible" @close = "signUpDilogVisible = false">
+                    我就是 slot 内容                  
+                </MyDialog>
+                <a href="#" class="button">登录</a>
+                <button class="button primary">保存</button>
+                <button class="button">预览</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import MyDialog from './MyDialog'
+export default {
+  name: 'Topbar',
+  data () {
+    return {
+      signUpDialogVisible: false
+    }
+  },
+  components: {
+    MyDialog
+  }
+}
+</script>
+
+<style scoped lang = "scss">
+    #Topbar {
+        background:#ffffff;
+        box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
+        >.wrapper {
+            min-width: 1024px;
+            max-width: 1440px;
+            margin: 0 auto ;
+            height: 64px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0 16px;
+        }
+        .logo {
+            font-size: 24px;
+            color: #000000;
+        }
+        .button {
+            width: 72px;
+            height: 32px;
+            border: none;
+            font-size: 18px;
+            background: ddd;
+            color: #222;
+            cursor: pointer;
+            text-decoration: none;
+            display: inline-flex;
+            justify-content: center;
+            align-items: center;
+            vertical-align: middle;
+            &:hover {
+                box-shadow: 1px 1px 1px hsla(0, 0, 0, 0.50);
+            }
+            &.primary {
+                background: #02af5f;
+                color: white;
+            }
+        }
+    }
+
+</style>
