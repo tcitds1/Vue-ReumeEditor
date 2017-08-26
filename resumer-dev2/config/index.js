756d8e02284457551d23326c0c7b33e5026b7d5b
konyahoshi <konyahoshi@qq.com>
2017-08-26 23:27:28 +0800

    生成预览

--- resumer-dev2/config/index.js
+++ resumer-dev2/config/index.js
@@ -8,7 +8,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     // assetsPublicPath: '/',
-    assetsPublicPath: '/Vue/resumer-dev2/dist',
+    assetsPublicPath: '/Vue/resumer-dev3/dist',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
